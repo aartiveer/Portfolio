48dca14fffd796e843e2d007f184836cb4d0ba6b
aartiveer <aartiveer1212@gmail.com>
2025-05-17 02:01:55 +0530

    Initial commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './HeroSection.css';
+
+
+const HeroSection = () => {
+    return (
+        <section className="hero">
+            <h1>Hiee, I'm Aarti  Veer</h1>
+            <p>I'm <b><i>Full Stack Developer</i></b></p>
+        </section>
+    );
+};
+
+export default HeroSection;
